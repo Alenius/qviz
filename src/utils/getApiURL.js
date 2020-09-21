bf5a7845e1e5e51766beb13809b9f653ad116dd0
Adam Alenius <adam.alenius@gmail.com>
2020-09-21 21:54:46 +0200

    feat: add public address to heroku

--- src/utils/getApiURL.js
+++ src/utils/getApiURL.js
@@ -3,6 +3,12 @@ export const getApiURL = (port = 4000) => {
     window.location.hostname === 'localhost' ||
     window.location.hostname === '127.0.0.1'
   const localhostUrl = `http://localhost:${port}`
-  const publicUrl = ''
-  return isLocalhost ? localhostUrl : publicUrl
+  const publicUrl = 'https://qviz-be.herokuapp.com/'
+  console.log({ process: process.env.REACT_APP_RUN_LOCALLY })
+  if (process.env.REACT_APP_RUN_LOCALLY) {
+    return localhostUrl
+  } else {
+    return publicUrl
+  }
+  // return isLocalhost ? localhostUrl : publicUrl
 }
