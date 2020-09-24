b613d6f60c4695ca3e6fd1a4d50c420da2376f48
Adam Alenius <adam.alenius@gmail.com>
2020-09-24 21:09:07 +0200

    style: handle direction flow of start page based on window width

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,28 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Typography, Layout, Space } from 'antd'
 import styled from 'styled-components'
 import { PageLayout } from '../components/PageLayout'
+import { WINDOW_SIZES } from '../constants'
+
+const StyledSpace = styled(Space)`
+  display: flex;
+  @media (max-width: ${WINDOW_SIZES.sm}px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
 
 export default function Start() {
   return (
     <PageLayout>
       <Space direction='vertical' align='center'>
         <Typography.Title>qviz</Typography.Title>
-        <Space>
+        <StyledSpace>
           <Typography.Text>Press this button to the quiz list</Typography.Text>
           <Button type='primary'>
             <Link to='/quiz/list'>Press here please</Link>
           </Button>
-        </Space>
-        <Space>
+        </StyledSpace>
+        <StyledSpace>
           <Typography.Text>
             Press this button to create a new quiz
           </Typography.Text>
           <Button type='primary'>
             <Link to='/create-quiz'>Create new</Link>
           </Button>
-        </Space>
+        </StyledSpace>
       </Space>
     </PageLayout>
   )
